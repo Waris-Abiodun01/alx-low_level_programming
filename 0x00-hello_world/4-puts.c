8a2a4dce21ebc75ed562ca3f53398a7c3dece6e0
Waris Abiodun <kazeemwarisabiodun05@gmail.com>
2022-09-08 05:56:11 -0700

    Waris

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - write a function that print programming ....
+ * Description : u are only allowed to use puts
+ * Return: the function should return 0 on sucess
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
+
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - write a function that print with proper ....
+ * Description : u are only allowed to use rintf
+ * Return: the function should return 0 on sucess
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+

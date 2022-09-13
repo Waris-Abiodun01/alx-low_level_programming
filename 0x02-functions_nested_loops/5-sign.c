0a6a6432aeca697b9a5497dd1d7c0a1390d31edc
Waris Abiodun <kazeemwarisabiodun05@gmail.com>
2022-09-13 07:03:39 -0700

    task7

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
  * _print_sign - check if the char is a lowercase
- * @c: character to be passed from main
+ * @n: character to be passed from main
  * Return: if it is lowercase return 1 , else return 0
  */
 int print_sign(int n);
 {
-	if (c > 0)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if(c == 0)
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - check if the char is a lowercase
+ * @c: character to be passed from main
+ * Return: if it is lowercase return 1 , else return 0
+ */
+int _abs(int c)
+{
+	if (c > 0)
+		return (c);
+	else
+		return (-1 * c);
+}

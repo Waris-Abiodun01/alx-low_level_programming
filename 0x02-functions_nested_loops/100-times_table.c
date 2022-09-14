fac0d130785266e7e5f36c3c2c4c4acbc0bc3e05
Waris Abiodun <kazeemwarisabiodun05@gmail.com>
2022-09-14 15:45:41 -0700

    addnatural

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * times_table - a function that print times tabe 0 to table 9
+ * @n :the  number to be printed is 15  or less
+ */
+void print_times_table(int n)
+{
+	int i = 0;
+	int j = 0;
+	int k;
+
+	if (n >= 0 && n < 16)
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			k = i * j;
+			if (j == 0)
+			{
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else if (k < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else if (k < 100)
+			{
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+			else
+			{
+
+				_putchar((k / 100) + '0');
+				_putchar(((k / 100) % 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+			if (j < n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - a function that add  the multiple 3 and 5 below 1024
+ * Return: 1 on success
+ */
+int main(void)
+{
+	int i = 1;
+	int k = 0;
+
+	while (i < 1024)
+	{
+		if (((i % 3) == 0) || ((i % 5) == 0))
+		{
+			k += i;
+		}
+		i++;
+	}
+	printf("%d\n", k);
+}

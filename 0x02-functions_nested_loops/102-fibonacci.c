ba4411764ade9b87400ae6a4f31e70f05c0befa3
Waris Abiodun <kazeemwarisabiodun05@gmail.com>
2022-09-14 16:52:21 -0700

    fibdone

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 
 /**
- * main - print first 50 fibonacci number 
+ * main - print first 50 fibonacci number
  * Return: 0 on success
  */
 int main(void)
 {
 	int i = 1;
-	int k = 1;
+	long int a = 0;
+	long int b = 1;
+	long int fib;
+
 	while (i <= 50)
 	{
-		k += i;
-		printf("%d, ", k);
+		fib = a + b;
+		a = b;
+		b = fib;
+
+		printf("%lu", fib);
+		if (i != 50)
+			printf(", ");
 		i++;
 	}
 	return (0);

a16e39dd177872894453f114bcb9b7ee9f69cac5
Waris Abiodun <kazeemwarisabiodun05@gmail.com>
2022-09-14 13:50:33 -0700

    multiplication

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,47 +6,31 @@ void times_table(void)
 {
 	int i = 0;
 	int j = 0;
-	int k ;
+	int k;
+
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
 			k = i * j;
-			if (i == 0)
+			if (j == 0)
 			{
 				_putchar(k + '0');
-				if ( j != 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
-
-			}
-			else if (k == 0)
-			{
-				_putchar(k + '0');
-				_putchar(',');
 			}
 			else if (k < 10)
 			{
 				_putchar(' ');
-				_putchar(' ');
 				_putchar(k + '0');
-				if (j != 9)
-				{
-					_putchar(',');
-				}
 			}
 			else if (k < 100)
 			{
-				_putchar(' ');
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
-				if (j != 9)
-				{
-					_putchar(',');
-				}
+			}
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
 		_putchar('\n');

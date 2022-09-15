158f763f6d7e2b8807a672113cc404aae00b28f2
Waris Abiodun <kazeemwarisabiodun05@gmail.com>
2022-09-14 17:28:51 -0700

    times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,51 +1,51 @@
+
 #include "main.h"
 /**
- * times_table - a function that print times tabe 0 to table 9
- * @n :the  number to be printed is 15  or less
+ * print_times_table - prints timetable only if < 15
+ * @n: use to input up to the timetable you want
+ * Description: i think i am getting into C
+ * Return: void and result
  */
 void print_times_table(int n)
 {
-	int i = 0;
-	int j = 0;
-	int k;
+	int row = 0, column, result;
 
-	if (n >= 0 && n < 16)
+	if (n < 0 || n > 15)
 		return;
-	for (i = 0; i <= n; i++)
+
+	while (row <= n)
 	{
-		for (j = 0; j <= n; j++)
+		for (column = 0; column <= n; column++)
 		{
-			k = i * j;
-			if (j == 0)
+			result = row * column;
+			if (column == 0)
+				_putchar(result + '0');
+			else if (result < 10)
 			{
 				_putchar(' ');
-				_putchar(k + '0');
+				_putchar(' ');
+				_putchar(result + '0');
 			}
-			else if (k < 10)
+			else if (result < 100)
 			{
 				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
-			}
-			else if (k < 100)
-			{
-				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
 			}
 			else
 			{
-
-				_putchar((k / 100) + '0');
-				_putchar(((k / 10) % 10) + '0');
-				_putchar((k % 10) + '0');
+				_putchar(result / 100 + '0');
+				_putchar((result - 100) / 10 + '0');
+				_putchar(result % 10 + '0');
 			}
-			if (j < n)
+
+			if (column < n)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
 		}
 		_putchar('\n');
+		row++;
 	}
 }

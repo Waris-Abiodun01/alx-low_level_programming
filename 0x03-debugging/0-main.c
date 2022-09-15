6f7bcbedd7da8cb8121dcdcc1fc97b841dd16424
Waris Abiodun <kazeemwarisabiodun05@gmail.com>
2022-09-15 12:15:53 -0700

    num

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,14 @@
 #include "main.h"
+
 /**
  * main - a function that pass a random number and check for postive or nega
  * Return: 0 on success
  */
 int main(void)
 {
-	int i = 0;
+	int i;
+
+	i = 0;
 
 	positive_or_negative(i);
 
